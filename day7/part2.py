0ed31344b2e452b56bc51da6701e30f5b5bd0d10
Chris Williamson <cjwilliamson109@gmail.com>
2016-12-08 14:25:43 +0000

    Cleaned up code for part 2 of day 7

--- day7/part2.py
+++ day7/part2.py
@@ -5,29 +5,29 @@ ip_addresses = []
 for line in input_file:
     ip_addresses.append(line.rstrip())
 
+def in_supernet(aba, ip):
+    # There are 3 regexes here (beacuse I don't know how to do all 3 in one)
+    # 1) Will match 0-* a-z/A-Z characters, then our aba match followed by 0-* a-z/A-Z characters then an open bracket e.g. yhjsabalop[
+    # 2) Will match ] then 0-* a-z/A-Z then our match then 0-* a-z/A-Z then [ e.g. ]dopabaerq[
+    # 1) Will match ] then 0-* a-z/A-Z characters, then our aba match followed by 0-* a-z/A-Z characters e.g. ]gyrabaors
+    # These three regexes cover all possibilities of the aba match being in the supernet sequence
+    return re.search(r'[a-zA-Z]*' + aba + '[a-zA-Z]*[\[]', ip) or re.search(r'[\]][a-zA-Z]*' + aba + '[a-zA-Z]*[\[]', ip) or re.search(r'[\]][a-zA-Z]*' + aba + '[a-zA-Z]*', ip)
+
+def in_hypernet(bab, ip):
+    return re.findall(r'[\[][a-zA-Z]*' + bab + '[a-zA-Z]*[\]]', ip)
+
 valid_ips = []
 
 for ip in ip_addresses:
     valid = False
     # Check if there are any ABA matches
-    abas = [''.join(match) for match in re.findall(r'(?=(\w)(\w)(\1))', ip)]
-    if ip == 'igefoemugshofmibco[uhahihzaglmzdpzjvfp]tfbuuhoughgismec[inbtuzxnxekfkulodyk]fxykxfkfnjvswwc':
-        print(abas)
+    abas = [''.join(match) for match in re.findall(r'(?=(\w)(\w)(\1))', ip)]    
     matches_index = 0
     while not valid and matches_index < len(abas):
         match = abas[matches_index]
-        # igefoemugshofmibco[uhahihzaglmzdpzjvfp]tfbuuhoughgismec[inbtuzxnxekfkulodyk]fxykxfkfnjvswwc
-        aba_in_supernet = re.search(r'[a-zA-Z]*' + match + '[a-zA-Z]*[\[]', ip) or re.search(r'[\]][a-zA-Z]*' + match + '[a-zA-Z]*[\[]', ip) or re.search(r'[\]][a-zA-Z]*' + match + '[a-zA-Z]*', ip)
-        if ip == 'igefoemugshofmibco[uhahihzaglmzdpzjvfp]tfbuuhoughgismec[inbtuzxnxekfkulodyk]fxykxfkfnjvswwc' and match == 'fkf':
-            print(aba_in_supernet)
-
         bab = match[1] + match[0] + match[1]
-        bab_in_hypernet = re.findall(r'[\[][a-zA-Z]*' + bab + '[a-zA-Z]*[\]]', ip)
-
-        if aba_in_supernet and bab_in_hypernet:
+        if in_supernet(match, ip) and in_hypernet(bab, ip):
             valid_ips.append(ip)
             valid = True
-            print(ip) 
-            # print(match, bab)
         matches_index += 1
 print(len(valid_ips))
